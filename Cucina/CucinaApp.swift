1320d1a89a7f3ebf55aa900504b694fe509c4c63
Bryant Pinto <bryant0845@gmail.com>
2025-03-18 17:22:55 -0700

    Initial Commit

---
+++ Cucina/CucinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CucinaApp.swift
+//  Cucina
+//
+//  Created by bryant pinto on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct CucinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CucinaTests/CucinaTests.swift
@@ -0,0 +1,16 @@
+//
+//  CucinaTests.swift
+//  CucinaTests
+//
+//  Created by bryant pinto on 3/18/25.
+//
+
+import Testing
+
+struct CucinaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
